dcbfbc5b3322e97cd2bd173e62ce020e28d3dd5c
Ian Hansson <ian.hansson@gmail.com>
2020-12-28 16:08:15 +0000

    array methods

---
+++ exercises/1-5.js
@@ -0,0 +1,59 @@
+function ex1(console) {
+    var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"],
+        counter;
+
+    for(counter = 0; counter < names.length; counter++) {
+        console.log(names[counter]);
+    }
+}
+
+function ex2(console) {
+    var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"];
+
+    names.forEach(function(name) {
+        console.log(name);
+    });
+}
+
+const releases = [
+    {
+        "id": 70111470,
+        "title": "Die Hard",
+        "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": [4.0],
+        "bookmark": []
+    },
+    {
+        "id": 654356453,
+        "title": "Bad Boys",
+        "boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": [5.0],
+        "bookmark": [{ id: 432534, time: 65876586 }]
+    },
+    {
+        "id": 65432445,
+        "title": "The Chamber",
+        "boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": [4.0],
+        "bookmark": []
+    },
+    {
+        "id": 675465,
+        "title": "Fracture",
+        "boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": [5.0],
+        "bookmark": [{ id: 432534, time: 65876586 }]
+    }
+];
+
+function ex3() {
+    return newReleases.map(({id, title}) => ({
+        id,
+        title
+    }));
+}
+
