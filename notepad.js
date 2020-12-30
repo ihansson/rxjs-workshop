614f8df50c26382e9971d400d5f7308ee347c222
Ian Hansson <ian.hansson@gmail.com>
2020-12-30 05:18:30 +0000

    reading api response

--- notepad.js
+++ notepad.js
@@ -1,9 +1,11 @@
 const rx = require("rxjs");
+const rxFetch = require("rxjs/fetch");
 const rxOps = require("rxjs/operators");
 
 // Number ops
 
 const { range, from, timer, of } = rx
+const { fromFetch } = rxFetch
 const { map, filter, pluck, distinct, mergeMap, switchMap, catchError } = rxOps
 const { log } = console
 
@@ -53,3 +55,27 @@ const example = timer(200).pipe(
 );
 
 const subscribe = example.subscribe(log);
+
+// Read
+
+const data$ = fromFetch('https://api.github.com/users?per_page=5').pipe(
+    switchMap(response => {
+        if (response.ok) {
+            // OK return data
+            return response.json();
+        } else {
+            // Server is returning a status requiring the client to try something else.
+            return of({ error: true, message: `Error ${response.status}` });
+        }
+    }),
+    catchError(err => {
+        // Network or other error, handle appropriately
+        console.error(err);
+        return of({ error: true, message: err.message })
+    })
+);
+
+data$.subscribe({
+    next: result => console.log(result),
+    complete: () => console.log('done')
+});
